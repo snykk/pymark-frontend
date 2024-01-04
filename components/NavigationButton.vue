55c7e14dcc12aa156209e9d57ccc33046ef403ba
Najib Fikri <najibfikri13@gmail.com>
2024-01-04 11:31:22 +0700

    refactor(file-sharing): Adjust styles and text in NavigationButton

--- components/NavigationButton.vue
+++ components/NavigationButton.vue
@@ -1,11 +1,27 @@
 <template>
     <NuxtLink :to="to" class="">
         <button
-            class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
+            :class="[
+                'relative inline-flex text-white items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium rounded-lg group focus:outline-none focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800 transition-all duration-300 ',
+                {
+                    'bg-gradient-to-br from-green-400 to-blue-600 hover:from-green-300 hover:to-blue-400': $route.path !== to,
+                    'bg-gradient-to-bl from-green-400 to-blue-600 hover:from-green-400 hover:to-blue-500': $route.path === to,
+                },
+            ]"
         >
-            <span class="relative py-2 px-4 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0"> {{ label }} </span>
-        </button></NuxtLink
-    >
+            <span
+                :class="[
+                    'relative py-2 px-4 rounded-md transition-all duration-300',
+                    {
+                        'bg-white dark:bg-gray-900': $route.path !== to,
+                        'bg-opacity-0': $route.path === to,
+                    },
+                ]"
+            >
+                {{ label }}
+            </span>
+        </button>
+    </NuxtLink>
 </template>
 
 <script setup lang="ts">
